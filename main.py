55932c1c71d4e8c0ee14b66314c6f3a5400b7293
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-04-01 09:42:06 +0900

    기본 main.py 파일 생성

---
+++ main.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import SGDRegressor # 확률적 경사 하강법 선형회귀 모델 (사용 가능성이 높은 모델)
+from sklearn.multioutput import MultiOutputRegressor # 다중 출력 회귀 모델 (사용 가능성이 높은 모델)
+import numpy as np # 배열 계산을 위한 넘파이
+
+class MAB: #멀티 암드 밴딧 알고리즘을 담은 클래스 생성
+    def __init__(self):
+        pass
+
+    def select_arn():
+        print("test") # 테스트 용 코드
+
+    def update():
+        print("test") # 테스트 용 코드
